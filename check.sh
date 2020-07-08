09ed5d024eb81a3f808749140bd0fa373da96f84
Érico Rolim <erico.erc@gmail.com>
2020-07-08 15:10:31 -0300

    check.sh: general fixes.
    Following @sgn's suggestions:
     - Remove subshells
     - Set MISSING to empty at the start
    
    Also added a debug message for mdbook invocation.

--- check.sh
+++ check.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
+# reset variable so it doesn't use the environment one
+MISSING=
+
 echo -n "Checking for mdbook-linkcheck ... "
-if ( ! command -v mdbook-linkcheck ); then
+if ! command -v mdbook-linkcheck; then
     MISSING="$MISSING mdbook-linkcheck"
     echo "not found"
 fi
 
 echo -n "Checking for vmdfmt ... "
-if ( ! command -v vmdfmt ); then
+if ! command -v vmdfmt; then
     MISSING="$MISSING vmdfmt"
     echo "not found"
 fi
@@ -15,22 +18,22 @@ fi
 if [ "$MISSING" ]; then
 
     echo -n "Checking for xbps-install ... "
-    if ( ! command -v xbps-install ); then
+    if ! command -v xbps-install; then
         echo "not found"
         echo "Please manually install: $MISSING"
         exit 1    
     fi
 
     echo -n "Checking for sudo ... "
-    if ( command -v sudo ); then
+    if command -v sudo; then
         SU_CMD='sudo'
     else
         echo "not found"
         echo "Checking for doas ... "
-        if ( command -v doas ); then
+        if command -v doas; then
             SU_CMD='doas'
         else
-	    echo "not found"
+            echo "not found"
             "Please manually install: $MISSING"
             exit 1
         fi
@@ -43,7 +46,7 @@ if [ "$MISSING" ]; then
     # Check whether executables are now present.
     
     for i in $MISSING; do
-        if ( ! command -v $i 2>&1 >/dev/null ); then
+        if ! command -v $i 2>&1 >/dev/null; then
             UNAVAILABLE="$UNAVAILABLE $i"
         fi
     done
@@ -57,9 +60,11 @@ fi
 
 vmdfmt -l -w src/
 
-if ( command -v mdbook 2>&1 >/dev/null ); then
+if command -v mdbook 2>&1 >/dev/null; then
+    echo "Building book and checking links with mdbook ..."
     mdbook build
 else
+    echo "Checking links with mdbook-linkcheck ..."
     mdbook-linkcheck -s
 fi
 
