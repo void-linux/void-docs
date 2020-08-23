219fa77873dc1cf1afbbeb187b7e76881b541241
Alexis <flexibeast@gmail.com>
2020-08-23 17:06:51 +1000

    <various>: Add cover page to PDFs.

---
+++ res/README.md
@@ -0,0 +1,2 @@
+The original cover variants, as suggested by @argosatcore on GitHub,
+are available in the `handbook-cover-variants.svg` file.
--- res/build.sh
+++ res/build.sh
@@ -28,8 +28,28 @@ sed -e "s,@PREFIX@,$PREFIX," res/void-docs.in > res/void-docs
 sed -e "s,@PREFIX@,$PREFIX," res/void-docs.1.in > res/void-docs.1
 
 # Build PDF
+
 echo "Building PDF"
+
+## Temporarily add OpenType version of Latin Modern to font cache.
+
+mkdir -p ~/.fonts
+TMP_FONTDIR=$(mktemp -d -p ~/.fonts)
+cp /usr/share/texmf-dist/fonts/opentype/public/lm/* $TMP_FONTDIR
+fc-cache
+
+## Create cover page using Latin Modern text.
+
+if [ -f res/handbook-cover.png ]; then
+    rm res/handbook-cover.png
+fi
+rsvg-convert \
+    --dpi-x 300 \
+    --dpi-y 300 \
+    res/handbook-cover.svg > res/handbook-cover.png
+
 ## Letter version
+
 pdflatex \
     -output-directory=book/latex/ \
     -jobname=handbook-letter \
@@ -40,7 +60,9 @@ pdflatex \
     -jobname=handbook-letter \
     book/latex/handbook.tex \
     >/dev/null
+
 ## A4 version
+
 sed -ie "s/\\documentclass\[letterpaper\]{article}/\\documentclass[a4paper]{article}/" \
     book/latex/handbook.tex
 pdflatex -output-directory=book/latex/ \
@@ -51,3 +73,8 @@ pdflatex -output-directory=book/latex/ \
          -jobname=handbook-a4 \
          book/latex/handbook.tex \
          >/dev/null
+
+## Remove temporary font directory
+
+rm -rf $TMP_FONTDIR
+fc-cache
