71abbd4b9c2f41058af97882b10511c5c42f9164
classabbyamp <void@placeviolette.net>
2022-07-10 17:03:44 -0400

    ci: fix summary check, make prints consistent
    without findutils, the summary check fails:
    `res/ci/check-summary.sh: 8: find: not found`

--- res/ci/format.sh
+++ res/ci/format.sh
@@ -2,25 +2,24 @@
 
 git config --global --add safe.directory "$PWD"
 
-echo "Checking links"
+printf "\033[37;1m=> Checking links\033[m\n"
 RUST_LOG=linkcheck=debug mdbook-linkcheck -s
 LINKCHECK=$?
 
 # Format them
-printf "Formatting tree"
+printf "\n\033[37;1m=> Formatting tree\033[m\n"
 vmdfmt -l -w src/
 
 # Check Status
 if [ ! -z "$(git status --porcelain)" ] ; then
     git diff
-    echo "Working directory not clean, files to be formatted:"
+    printf "\033[31;1m=> Working directory not clean, files to be formatted:\033[m\n"
     git status
     VMDFMT=1
 fi
 
 # Check SUMMARY.md
-echo ""
-echo "Checking SUMMARY.md"
+printf "\n\033[37;1m=> Checking SUMMARY.md\033[m\n"
 res/ci/check-summary.sh
 SUMMARY=$?
 
