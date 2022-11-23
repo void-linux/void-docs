feb2d0531bcd2783763913edf7327ff736ca7760
classabbyamp <void@placeviolette.net>
2022-11-23 00:27:49 -0500

    res/ci: split CI steps into separate scripts
    also various other improvements

--- res/ci/format.sh
+++ res/ci/format.sh
@@ -1,29 +1,15 @@
 #!/bin/sh
 
-git config --global --add safe.directory "$PWD"
-
-printf "\033[37;1m=> Checking links\033[m\n"
-RUST_LOG=linkcheck=debug mdbook-linkcheck -s
-LINKCHECK=$?
-
-# Format them
 printf "\n\033[37;1m=> Formatting tree\033[m\n"
 vmdfmt -l -w src/
 
 # Check Status
 if [ ! -z "$(git status --porcelain)" ] ; then
-    git diff
-    printf "\033[31;1m=> Working directory not clean, files to be formatted:\033[m\n"
-    git status
-    VMDFMT=1
-fi
-
-# Check SUMMARY.md
-printf "\n\033[37;1m=> Checking SUMMARY.md\033[m\n"
-res/ci/check-summary.sh
-SUMMARY=$?
-
-# Generate exit value
-if [ ! -z $VMDFMT ] || [ ! $LINKCHECK -eq 0 ] || [ ! $SUMMARY -eq 0 ] ; then
-    exit 2
+    git diff --color=always
+    printf "\033[31;1m=> Files which need to be formatted:\033[m\n"
+    for f in $(git status | grep -Po 'modified:\K.*$'); do
+        printf "$f\n"
+        printf "::error title=Formatting Lint,file=$f,line=1::File has improper formatting\n"
+    done
+    exit 1
 fi
